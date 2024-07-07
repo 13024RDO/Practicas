2fd8c4e3a5972c6cf570f5fe20d73915c61da2f2
Leonardo <leoonardoogzz@gmail.com>
2024-07-07 19:26:54 -0300

    Se elimino proyecto juego y se agrego el contador

---
+++ CuentaRegresiva/app.js
@@ -0,0 +1,11 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+const newYear = '1 Enero 2025';
+
+function countTimer(){
+    const newYearDate = new Date(newYear);
+    
+}
