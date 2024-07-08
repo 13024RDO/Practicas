062ab230e3956a6a6ac9f0ace6c6ce7d34989698
Leonardo <leoonardoogzz@gmail.com>
2024-07-08 16:01:49 -0300

    Se termino el proyecto de cronometro

--- CuentaRegresiva/app.js
+++ CuentaRegresiva/app.js
@@ -3,9 +3,24 @@ const hours = document.getElementById("hours");
 const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
 
-const newYear = '1 Enero 2025';
+const newYear = 'January 1, 2025';  // Corregido el formato de la fecha
 
-function countTimer(){
+function countTimer() {
     const newYearDate = new Date(newYear);
-    
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;  // Corregido el cálculo de totalSeconds
+
+    const daysCalc = Math.floor(totalSeconds / 3600 / 24);
+    const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
+    const minsCalc = Math.floor(totalSeconds / 60) % 60;
+    const secondsCalc = Math.floor(totalSeconds) % 60;  // Corregido el cálculo de secondsCalc
+
+    days.innerHTML = daysCalc;
+    hours.innerHTML = hoursCalc;
+    mins.innerHTML = minsCalc;
+    seconds.innerHTML = secondsCalc;
 }
+
+countTimer();
+setInterval(countTimer, 1000);
