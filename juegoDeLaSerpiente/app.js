45292e9e30838fde666c7220fb5100ab3adfb4da
Leonardo <leoonardoogzz@gmail.com>
2024-07-16 14:39:06 -0300

    Se creo la cabeza de la serpiente

--- juegoDeLaSerpiente/app.js
+++ juegoDeLaSerpiente/app.js
@@ -1,16 +1,19 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
+let snakeX = 5, snakeY = 10;
 
 const changFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 };
 
-const initGame = () => {
-    changFoodPosition();  // Llama a la función para cambiar la posición de la comida
+const initGame = () => { 
+    changFoodPosition(); 
     let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
+    htmlMarkup += `<div class="head" style="grid-area:${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 };
+
 
 initGame();
